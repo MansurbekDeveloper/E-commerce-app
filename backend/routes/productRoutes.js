64f9212336dc3134d4dbf33041ee431b6c123416
MansurbekDeveloper <cool.mansurbek@bk.ru>
2021-01-14 11:24:44 +0500

    first commit

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getProducts,
+  getProductById,
+} = require("../controller/productControllers");
+
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+
+module.exports = router;
